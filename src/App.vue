06cb4a7715ff9b7aef202b87a2ded27cda615a21
Hongzf <1628415507@qq.com>
2022-07-22 09:38:49 +0800

    feat：vue配置

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<!--
+ * @Author: Hongzhifeng
+ * @Date: 2022-07-22 09:31:09
+ * @LastEditors: Hongzhifeng
+ * @LastEditTime: 2022-07-22 09:31:31
+ * @Description: 
+-->
+<template>vue3不再需要根元素了
+
+</template>
